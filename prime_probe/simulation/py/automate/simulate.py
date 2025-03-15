540f5bb43f9bc2802de7de8f5f880a064c29014c
James Chuang <lchuang@cs.unc.edu>
2025-03-14 21:27:42 -0400

    add estimate test length function

--- prime_probe/simulation/py/automate/simulate.py
+++ prime_probe/simulation/py/automate/simulate.py
@@ -54,6 +54,23 @@ def scan_keystrokes(data):
             not_found_keys.append(keystroke)
     return not_found_keys
 
+def get_total_duration(data):
+    duration_ms = 0
+    for sentence in data:
+       duration_ms += sum(sentence["intervals"]) 
+    
+    duration_s = duration_ms // 1000
+    duration_ms -= duration_s * 1000
+    duration_min = duration_s // 60
+    duration_s -= duration_min * 60
+    duration_hr = duration_min // 60
+    duration_min -= duration_hr * 60
+    duration_day = duration_hr // 24
+    duration_hr -= duration_day * 24
+    return [duration_day, duration_hr, duration_min, duration_s, duration_ms]
+
+
+
 def debugPrint(data):
     durations = [sum(obj["intervals"]) for obj in data]
     print(durations)
@@ -80,7 +97,9 @@ if __name__ == "__main__":
 
         data = load_json(f"simulation/data/cleaned_data/{file}.jsonl")
         nonmapped_keys = scan_keystrokes(data)
+        total_duration = get_total_duration(data)
         print(f"nonmapped_keys: {nonmapped_keys}")
+        print(f"The test is estimated to take {total_duration[0]} days {total_duration[1]} hours {total_duration[2]} minutes {total_duration[3]} seconds {total_duration[4]} milliseconds")
         # fast-forward mechanism within a file
         start_index = 0
         if len(start_id) != 0:
@@ -143,8 +162,9 @@ if __name__ == "__main__":
             # load data from file
             data = load_json(f"simulation/data/cleaned_data/{filename}")
             nonmapped_keys = scan_keystrokes(data)
+            total_duration = get_total_duration(data)
             print(f"nonmapped_keys: {nonmapped_keys}")
-
+            print(f"The test is estimated to take {total_duration[0]} days {total_duration[1]} hours {total_duration[2]} minutes {total_duration[3]} seconds {total_duration[4]} milliseconds")
             for sentence in data:
                 sentence_id = f"{sentence["participant_id"]}-{sentence["test_section_id"]}-{sentence["sentence_id"]}"
                 # initialize simulation state and acknowledge
